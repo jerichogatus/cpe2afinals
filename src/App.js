7f11e48d5fa3be1a0207dffeea5589c6a8b84eb7
rnvelasco <rnvelasco@yahoo.com>
2024-04-25 14:43:35 +0800

    Added addition and subtraction funtionalities.

--- src/App.js
+++ src/App.js
@@ -23,42 +23,83 @@ function CalcDisplay({display}) {
 export default function App() {
 
   const[disp, setDisp] = useState(0);
+  
+  const[num1, setNum1] = useState(null);
+  const[num2, setNum2] = useState(null);
+  const[op, setOp] = useState(null);
 
-  const clickHandler = (e) => {
-    e.preventDefault();
-    const value  = e.target.innerHTML;
-    setDisp(value);
-    //alert('You clicked: ' + value);
+  const clrClickHandler = (e) => {
+    setDisp(0);
+    setNum1(null);
+    setNum2(null);
+    setOp(null);
+  }
+
+  const equalClickHandler = (e) => {
+    console.log('Num1 ' + num1 + ' | ' + 'Op ' + op + ' | ' + 'Num2 ' + num2);
+    if (op === "+") {
+      setDisp(parseInt(num1) + parseInt(num2))
+    } else if (op === "-") {
+      setDisp(parseInt(num1) - parseInt(num2))
+    } else {
+      setDisp('Invalid Operation');
+    }
   }
 
   const numberClickHandler = (e) => {
     e.preventDefault();
     const value  = e.target.innerHTML;
-    setDisp(value);
-    //alert('You clicked: ' + value);
+
+    if (op === null) {
+      if(num1 === null) {
+        setNum1(value);
+        setDisp(value);
+      } else {
+        setNum1(num1 + value);
+        setDisp(num1 + value);
+      }
+      
+    } else {
+      if(num2 === null) {
+        setNum2(value);
+        setDisp(value);
+      } else {
+        setNum2(num2 + value);
+        setDisp(num2 + value);
+      }
+    }
+    
   }
+
+  const opClickHandler = (e) => {
+    e.preventDefault();
+    const value  = e.target.innerHTML;
+    setOp(value);
+    setDisp(value);
+  }
+
 
   return (
     <div className="App">
       <div className="CalcContainer">
         <CalcDisplay display={disp} />
         <div className="ButtonContainer">
-          <CalcButton label={"/"} onClick={clickHandler}/>
+          <CalcButton label={"÷"} onClick={opClickHandler}/>
           <CalcButton label={7} onClick={numberClickHandler}/>
           <CalcButton label={8} onClick={numberClickHandler}/>
           <CalcButton label={9} onClick={numberClickHandler}/>
-          <CalcButton label={"*"} onClick={clickHandler}/>
+          <CalcButton label={"x"} onClick={opClickHandler}/>
           <CalcButton label={4} onClick={numberClickHandler}/>
           <CalcButton label={5} onClick={numberClickHandler}/>
           <CalcButton label={6} onClick={numberClickHandler}/>
-          <CalcButton label={"+"} onClick={clickHandler}/>
+          <CalcButton label={"+"} onClick={opClickHandler}/>
           <CalcButton label={1} onClick={numberClickHandler}/>
           <CalcButton label={2} onClick={numberClickHandler}/>
           <CalcButton label={3} onClick={numberClickHandler}/>
-          <CalcButton label={"-"}  onClick={clickHandler}/>
-          <CalcButton label={"C"} onClick={clickHandler}/>
+          <CalcButton label={"-"}  onClick={opClickHandler}/>
+          <CalcButton label={"CLR"} onClick={clrClickHandler}/>
           <CalcButton label={0} onClick={numberClickHandler}/>
-          <CalcButton label={"="} onClick={clickHandler}/>
+          <CalcButton label={"="} onClick={equalClickHandler}/>
         </div>
       </div>
     </div>
